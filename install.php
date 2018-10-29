53c2c2b381754bf2de7fe45e8c5cbf7f84f9f18d
Suraj Nagendra <surajnagendra@gmail.com>
2018-10-29 20:23:43 +0530

    Update install.php

--- install.php
+++ install.php
@@ -16,7 +16,8 @@ define('DRUPAL_ROOT', getcwd());
 define('MAINTENANCE_MODE', 'install');
 
 // Exit early if running an incompatible PHP version to avoid fatal errors.
-if (version_compare(PHP_VERSION, '5.2.4') < 0) {
+if (version_compare(PHP_VERSION, '5.2.4') < 0) 
+{
   print 'Your PHP installation is too old. Drupal requires at least PHP 5.2.4. See the <a href="http://drupal.org/requirements">system requirements</a> page for more information.';
   exit;
 }
